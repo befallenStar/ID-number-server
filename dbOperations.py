37dd979e2fe3b819f3e1853cc5d6d21ecbeaecf7
Junenatte-and-little-pig <sy5622_5@126.com>
2020-10-05 21:43:20 +0800

    complete sql operation

--- dbOperations.py
+++ dbOperations.py
@@ -1,10 +1,14 @@
 # -*- encoding: utf-8 -*-
 import pymysql
+from scratchData import load_data
+
+municipality = ['11', '12', '31', '50']
+
 
 
 def main():
     conn=pymysql.connect(host='localhost',user='root',password='root',database='id_number',charset='utf8')
-    cursor=conn.cursor()
+    cursor=conn.cursor(cursor=pymysql.cursors.DictCursor)
     # sql='''
     # CREATE TABLE provinces(
     # id INT auto_increment primary key ,
@@ -21,15 +25,35 @@ def main():
     # municipality bool not null default 0
     # )ENGINE=innodb DEFAULT CHARSET=utf8;
     # '''
-    sql='''
-    CREATE TABLE districts(
-    id INT auto_increment primary key ,
-    code char(6) not null ,
-    city char(20) not null ,
-    city_id int not null
-    )ENGINE=innodb DEFAULT CHARSET=utf8;
-    '''
-    res=cursor.execute(sql)
+    # sql='''
+    # CREATE TABLE districts(
+    # id INT auto_increment primary key ,
+    # code char(6) not null ,
+    # city char(20) not null ,
+    # city_id int not null
+    # )ENGINE=innodb DEFAULT CHARSET=utf8;
+    # '''
+    provinces,cities,districts=load_data()
+    # for code,city in provinces.items():
+    #     print(code,city)
+    #     sql='''insert into provinces (code,city) values ('%s','%s');'''%(code,city)
+    #     res=cursor.execute(sql)
+    #     print(res)
+
+    for code, city in cities.items():
+        sql='''select * from provinces where code = %s'''
+        res=cursor.execute(sql,[code[:2]])
+        if res:
+            # print(cursor.fetchone())
+            result=cursor.fetchone()
+            muni=0
+            if result['code'] in municipality:
+                muni=1
+            # print(code, city)
+            sql = '''insert into cities (code,city,province_id,municipality) values (%s,%s,%s,%s);'''
+            res = cursor.execute(sql,[code,city,result['id'],muni])
+            # print(res)
+    conn.commit()
     cursor.close()
     conn.close()
 
--- scratchData.py
+++ scratchData.py
@@ -17,20 +17,15 @@ def load_data():
         code = re.findall('\d{6}', code_list[step])[0]
         city = re.findall('[\u4e00-\u9fa5]+', code_list[step + 1])[0]
         if code[2:] == '0000':
-            if code[0] not in provinces.keys():
-                provinces[code[0]] = {}
-            provinces[code[0]][code[:2]] = city
+            provinces[code[:2]] = city
         elif code[4:] == '00':
-            if code[:2] not in cities.keys():
-                cities[code[:2]] = {}
-            cities[code[:2]][code[:4]] = city
+            cities[code[:4]] = city
         else:
             if code[:2] in municipality:
-                if code[:2] not in cities.keys():
-                    cities[code[:2]] = {}
-                cities[code[:2]][code] = city
+                cities[code] = city
             else:
-                if code[:4] not in districts.keys():
-                    districts[code[:4]] = {}
-                districts[code[:4]][code] = city
+                districts[code] = city
+    # provinces = {'11' : '北京市', ...}
+    # cities = {'110101' : '东城区', ..., '1301' : '石家庄市', ...}
+    # districts = {'130102' : '长安区', ...}
     return provinces, cities, districts
